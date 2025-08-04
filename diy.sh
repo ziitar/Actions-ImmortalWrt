10287cbe6d1047555b9f02dea5aa31019fb9dff0
ziitar <33385784+ziitar@users.noreply.github.com>
2025-08-04 18:55:27 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,4 +2,6 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+
 git clone https://github.com/SunBK201/UA3F.git package/UA3F
